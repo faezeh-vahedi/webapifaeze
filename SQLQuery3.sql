6ae5a13e95710c165896d3f66fb1a84199be1b7f
faezeh-vahedi <118221831+faezeh-vahedi@users.noreply.github.com>
2022-12-03 20:36:45 +0330

    add request response

---
+++ SQLQuery3.sql
@@ -0,0 +1,20 @@
+USE [SocialDb]
+GO
+/****** Object:  Table [dbo].[Post]    Script Date: 9/20/2022 12:22:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Post](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[Description] [nvarchar](1000) NOT NULL,
+	[Imagepath] [nvarchar](255) NULL,
+	[TS] [smalldatetime] NOT NULL,
+	[Published] [bit] NOT NULL,
+ CONSTRAINT [PK_Post] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
